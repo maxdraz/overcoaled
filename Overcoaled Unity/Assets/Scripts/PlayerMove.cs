5f80128dc3ecfdc31403b062d8b93c09ba6fe51f
Aaron <aaronjhamilton1998@gmail.com>
2019-02-19 20:38:25 +0000

    Finished movement (for now)

--- Overcoaled Unity/Assets/Scripts/PlayerMove.cs
+++ Overcoaled Unity/Assets/Scripts/PlayerMove.cs
@@ -11,13 +11,17 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         float posX = Input.GetAxis("joystick " + playerNumber + " horizontal") * moveSpeed;
-        float posY = Input.GetAxis("joystick " + playerNumber + " vertical") * moveSpeed;
+        float posY = Input.GetAxis("joystick " + playerNumber + " vertical") * -moveSpeed;
 
-        Vector3 direction = new Vector3(posX, 0.0f, posY);
-        transform.rotation = Quaternion.LookRotation(-direction);
+        if ((Input.GetAxis("joystick " + playerNumber + " horizontal") > 0.2 || Input.GetAxis("joystick " + playerNumber + " horizontal") < -0.2)
+            || (Input.GetAxis("joystick " + playerNumber + " vertical") > 0.2 || Input.GetAxis("joystick " + playerNumber + " vertical") < -0.2))
+        {
+            Vector3 direction = new Vector3(posX, 0.0f, posY);
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
 
-        transform.Translate(Vector3.right * posX * Time.deltaTime);
-        transform.Translate(Vector3.forward * posY * Time.deltaTime);
+        transform.Translate(Vector3.right * posX * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.forward * posY * Time.deltaTime, Space.World);
         
     }
 
