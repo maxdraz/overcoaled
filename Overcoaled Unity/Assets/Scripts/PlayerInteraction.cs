70b261290ef53f415f8e9a45110053bdaf7e6c0b
Max <38158634+maxdraz@users.noreply.github.com>
2019-02-19 20:21:28 +0000

    working on player interaction

---
+++ Overcoaled Unity/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] private bool isCarrying = false;
+    [SerializeField] private bool carryingPlank = false;
+    [SerializeField] private bool carryingCoal = false;
+    [SerializeField] private bool carryingAmmo = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if colliding with plank box and not carrying anything
+        if(other.tag == "Plank Box" && !isCarrying)
+        {
+            PickUpPlank();
+        }
+
+
+    }
+
+    void PickUpPlank()
+    {
+        isCarrying = true;
+        carryingPlank = true;
+    }
+}
