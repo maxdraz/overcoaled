b3177985b1b5c25249966a7212b09fcded9dae8a
Max <38158634+maxdraz@users.noreply.github.com>
2019-02-19 13:52:26 +0000

    Added more folders and walls

---
+++ Overcoaled Unity/Assets/Scripts/Wall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public float health = 3f;    
+
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+    }
+
+    // JUST FOR TESTING
+    private void OnMouseDown()
+    {
+        Debug.Log("hit!");
+        TakeDamage(1f);
+    }
+}
