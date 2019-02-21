88f2d4903350c032b08a959cf7013fc09c66f15b
Max <38158634+maxdraz@users.noreply.github.com>
2019-02-21 15:15:09 +0000

    Changes to bullet

--- Overcoaled Unity/Assets/Scripts/ThrownObjectMove.cs
+++ Overcoaled Unity/Assets/Scripts/ThrownObjectMove.cs
@@ -8,6 +8,7 @@ public class ThrownObjectMove : MonoBehaviour
     public float forceScale = 8f;
     [SerializeField] private bool grounded = false;
     public int groundLayerIndex;
+    public GameObject groundHitPS;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +22,11 @@ public class ThrownObjectMove : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == groundLayerIndex)
+        if (collision.gameObject.layer == groundLayerIndex && !grounded)
         {
-
+            print(collision.gameObject.name);
             grounded = true;
+            GameObject ps = (GameObject)Instantiate(groundHitPS, transform.position, Quaternion.identity);
         }
     }
 
