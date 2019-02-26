01e4f1be22103e497de34c87b9483267c091ff53
Max <38158634+maxdraz@users.noreply.github.com>
2019-02-26 13:42:33 +0000

    working on multiple target cam

---
+++ Overcoaled Unity/Assets/Scripts/MultipleTargetCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MultipleTargetCamera : MonoBehaviour
+{
+
+    public List<Transform> targets;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        if (targets.Count == 0)
+        {
+            return;
+        }
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = newPosition;
+    }
+
+    private void Move()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+
+        Move();
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+
+        for(int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
+    }
+}
